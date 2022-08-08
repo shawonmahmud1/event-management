3660b8c99c3f25ecce46c13a681b56c158bb1937
shawonmahmud1 <alisterdavidson79@gmail.com>
2022-08-08 23:38:24 +0600

    relax

--- js/events.js
+++ js/events.js
@@ -6,3 +6,22 @@ blueButton.onclick = makeBlue;
 function makeBlue() {
   document.body.style.background = "blue";
 }
+const greenBtn = document.getElementById("green-btn");
+greenBtn.onclick = function makeGreen() {
+  document.body.style.backgroundColor = "green";
+};
+const goldenRod = document.getElementById("make-goldenrod");
+goldenRod.addEventListener("click", makeGoldenRod);
+
+function makeGoldenRod() {
+  document.body.style.backgroundColor = "goldenrod";
+}
+const makeHotPink = document.getElementById("hotpink");
+makeHotPink.addEventListener("click", function pink() {
+  document.body.style.backgroundColor = "hotpink";
+});
+document
+  .getElementById("light-blue")
+  .addEventListener("click", function lightblue() {
+    document.body.style.backgroundColor = "lightblue";
+  });
